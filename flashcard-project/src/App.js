b668b779896160be1bb9dc8e0a2540e83630a4e2
mwx1993 <mawenxin52@gmail.com>
2017-04-24 17:05:07 -0400

    implement time, and set up the container

--- flashcard-project/src/App.js
+++ flashcard-project/src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render() {
+    const current_word = {
+      word: 'Code Injection'
+    };
+
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="progress-bar">{this.state.date.toLocaleTimeString()}</div>
+        <div className="word-content">
+          <h1>{current_word.word}</h1>
+          <div className="divide-line"></div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
+
 }
 
 export default App;
